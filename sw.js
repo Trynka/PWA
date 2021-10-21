cc7596e83be47ddc48ea6e6f03349203807dab89
Desislava Ivanova <dessislava.iwanowa@gmail.com>
2021-10-21 14:26:43 +0300

    chore: dynamic cache added

--- sw.js
+++ sw.js
@@ -1,35 +1,36 @@
 const staticCacheName = "site-static-v2";
+const dynamicCache = "site-dynamic-v1";
 const assets = [
-  "/",
-  "/index.html",
-  "/js/app.js",
-  "/js/ui.js",
-  "/js/materialize.min.js",
-  "/css/styles.css",
-  "/css/materialize.min.css",
-  "/img/dish.png",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v111/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+    "/",
+    "/index.html",
+    "/js/app.js",
+    "/js/ui.js",
+    "/js/materialize.min.js",
+    "/css/styles.css",
+    "/css/materialize.min.css",
+    "/img/dish.png",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://fonts.gstatic.com/s/materialicons/v111/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
 ];
 
 self.addEventListener("install", (evt) => {
     //console.log("service worker installed", evt)
     evt.waitUntil(
-        caches.open(staticCacheName)
-            .then((cache) => {
-                cache.addAll(assets);
-            })
+        caches.open(staticCacheName).then((cache) => {
+            cache.addAll(assets);
+        })
     );
 });
 
 self.addEventListener("activate", (evt) => {
     //console.log("service worker activated", evt);
     evt.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== staticCacheName)
-                .map(key => caches.delete(key))
-            )
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys
+                    .filter((key) => key !== staticCacheName)
+                    .map((key) => caches.delete(key))
+            );
         })
     );
 });
@@ -37,8 +38,13 @@ self.addEventListener("activate", (evt) => {
 self.addEventListener("fetch", (evt) => {
     //console.log("fetch event", evt);
     evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+        caches.match(evt.request).then((cacheRes) => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
         })
-    )
+    );
 });
