603125b028f1a63f19e13e0102338b9902dd3d13
Desislava Ivanova <dessislava.iwanowa@gmail.com>
2021-10-21 13:14:28 +0300

    chore: updating cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = "site-static";
+const staticCacheName = "site-static-v2";
 const assets = [
   "/",
   "/index.html",
@@ -23,7 +23,15 @@ self.addEventListener("install", (evt) => {
 });
 
 self.addEventListener("activate", (evt) => {
-    console.log("service worker activated", evt);
+    //console.log("service worker activated", evt);
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 });
 
 self.addEventListener("fetch", (evt) => {
