00e95800eb66ee127e7e9cbd73e20af9a880cd31
Desislava Ivanova <dessislava.iwanowa@gmail.com>
2021-10-21 10:33:28 +0300

    chore: getting cash

--- sw.js
+++ sw.js
@@ -1,3 +1,36 @@
-self.addEventListener("install", evt => console.log("service worker installed", evt))
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v111/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+];
 
-self.addEventListener("activate", evt => console.log("service worker activated", evt))
+self.addEventListener("install", (evt) => {
+    //console.log("service worker installed", evt)
+    evt.waitUntil(
+        caches.open(staticCacheName)
+            .then((cache) => {
+                cache.addAll(assets);
+            })
+    );
+});
+
+self.addEventListener("activate", (evt) => {
+    console.log("service worker activated", evt);
+});
+
+self.addEventListener("fetch", (evt) => {
+    //console.log("fetch event", evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    )
+});
